0c84ebe7eb9e94569aeccbd28666534c8948352e
mikepreston17 <michael.n.preston@gmail.com>
2018-10-09 23:53:54 -0500

    added core logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,66 @@
-var goal;
+/*	Author: Michael Preston
+ *	Date: "10-09-2018"
+ */
+var goal, totalScore, wins, losses;
 
+var gemDictionary = {
+    ruby: 10,
+    sapphire: 5,
+    topaz: 1,
+    emerald: 2,
+}
 
+window.addEventListener('load', init)
 
+$(document).on("click", ".gem", function () {
+    let score = gemDictionary[this.alt];
 
+    if (typeof score !== 'number') {
+        throw new Error('score was not a number! Could not update score!')
+    }
 
+    totalScore += score;
+    render()
+    checkWinCondition()
+})
+
+function checkWinCondition() {
+    if (totalScore < goal)
+        return;
+    else if (totalScore == goal) {
+        alert('You win!')
+        wins++;
+    } else if (totalScore > goal) {
+        alert('You lose!')
+        losses++;
+    }
+    reset();
+}
 
 function init() {
-    goal = randomInt(19, 120, true); //I surmised this would be inclusive.
+    totalScore = 0;
+    losses = 0;
+    wins = 0;
+    setGoal();
+    render();
+}
+
+function setGoal() {
+    goal = randomInt(19, 120, true);
     $('#goal').text(goal);
 }
 
+function reset() {
+    totalScore = 0;
+    setGoal();
+}
+
 function randomInt(min, max, inclusive) {
-    return Math.floor(Math.random() * (max - min + inclusive ? 1 : 0)) + min;
+    return Math.floor(Math.random() * (max - min + (inclusive ? 1 : 0))) + min
+}
+
+function render() {
+    $('#score').text(totalScore);
+    $('#losses').text(losses);
+    $('#wins').text(wins);
 }
